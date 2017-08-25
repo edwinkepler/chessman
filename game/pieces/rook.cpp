bd25e376dd0b6d4ca7ad2d48fe9fc998b37dc592
edwinkepler <edwinkepler@protonmail.com>
2017-08-25 19:03:32 +0200

    Add implementation of Rook::identify_move() method

--- game/pieces/rook.cpp
+++ game/pieces/rook.cpp
@@ -11,50 +11,42 @@ namespace Chessman
 
     const int Rook::identify_move(
         const pair<int, int>& to,
-        const vector<vector<Chessman::Piece*> >&)
+        const vector<vector<Chessman::Piece*> >& vb)
     {
-        // if(
-        //     getLastMove().first == to.first &&
-        //     getLastMove().second != to.second)
-        // {
-        //     for(int i = 1; i <= abs(getLastMove().second - to.second); i++) {
-        //         int y = (to.second - getLastMove().second) < 0 ? getLastMove().second - i : getLastMove().second + i;
-        //         if(
-        //             !b->isSquareEmpty(make_pair(to.first, y)) &&
-        //             i == abs(getLastMove().second - to.second))
-        //         {
-        //             if(b->getPiece(make_pair(to.first, y))->getOwner() != getOwner()) {
-        //                 return Chessman::MOVES::CAPTURE;
-        //             } else {
-        //                 return Chessman::MOVES::INVALID;
-        //             }
-        //         } else if(!b->isSquareEmpty(make_pair(to.first, y))) {
-        //             return Chessman::MOVES::INVALID;
-        //         }
-        //     }
-        //     return Chessman::MOVES::VALID;
-        // } else if(
-        //     getLastMove().second == to.second &&
-        //     getLastMove().first != to.first)
-        // {
-        //     for(int i = 1; i <= abs(getLastMove().first - to.first); i++) {
-        //         int x = (to.first - getLastMove().first) < 0 ? getLastMove().first - i : getLastMove().first + i;
-        //         if(
-        //             !b->isSquareEmpty(make_pair(x, to.second)) &&
-        //             i == abs(getLastMove().first - to.first))
-        //         {
-        //             if(b->getPiece(make_pair(x, to.second))->getOwner() != getOwner()) {
-        //                 return Chessman::MOVES::CAPTURE;
-        //             } else {
-        //                 return Chessman::MOVES::INVALID;
-        //             }
-        //         } else if(!b->isSquareEmpty(make_pair(x, to.second))) {
-        //             return Chessman::MOVES::INVALID;
-        //         }
-        //     }
-        //     return Chessman::MOVES::VALID;
-        // } else {
-        //     return Chessman::MOVES::INVALID;
-        // }
+        int x1 = last_move().first;
+        int y1 = last_move().second;
+        int x2 = to.first;
+        int y2 = to.second;
+        if(x1 == x2 && y1 != y2) {
+            for(int i = 1; i <= abs(y1 - y2); i++) {
+                int yi = (y2 - y1) < 0 ? y1 - i : y1 + i;
+                if(vb[yi - 1][vb.size() - x1] != nullptr && i == abs(y1 - y2)) {
+                    if(vb[yi - 1][vb.size() - x1]->owner() != owner()) {
+                        return Chessman::MOVES::CAPTURE;
+                    } else {
+                        return Chessman::MOVES::INVALID;
+                    }
+                } else if(vb[yi - 1][vb.size() - x1] != nullptr) {
+                    return Chessman::MOVES::INVALID;
+                }
+            }
+            return Chessman::MOVES::VALID;
+        } else if(y1 == y2 && x1 != x2) {
+            for(int i = 1; i <= abs(x1 - x2); i++) {
+                int xi = (x2 - x1) < 0 ? x1 - i : x1 + i;
+                if(vb[y2 - 1][vb.size() - xi] != nullptr && i == abs(x1 - x2)) {
+                    if(vb[y2 - 1][vb.size() - xi]->owner() != owner()) {
+                        return Chessman::MOVES::CAPTURE;
+                    } else {
+                        return Chessman::MOVES::INVALID;
+                    }
+                } else if(vb[y2 - 1][vb.size() - xi] != nullptr) {
+                    return Chessman::MOVES::INVALID;
+                }
+            }
+            return Chessman::MOVES::VALID;
+        } else {
+            return Chessman::MOVES::INVALID;
+        }
     }
 }
