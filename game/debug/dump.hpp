d25b589ff481faeecce6062fd2230e836b2c4cbc
edwinkepler <edwinkepler@protonmail.com>
2017-09-01 08:35:08 +0200

    Add Debug::Dump class

---
+++ game/debug/dump.hpp
@@ -0,0 +1,47 @@
+/**
+ * @copyright 2017 Edwin Kepler
+ * @license MIT
+ */
+
+#ifndef DUMP_HPP
+#define DUMP_HPP
+
+#include <iostream>
+
+#include "pieces/piece.hpp"
+#include "board.hpp"
+
+using namespace std;
+
+class Board;
+class Piece;
+
+namespace Debug
+{
+    class Dump {
+    public:
+        /**
+        * @brief Print all information about Chessboard::Board.
+        * @param Pointer to Chessboard::Board object.
+        */
+        void board(Chessboard::Board*);
+        /**
+        * @brief Print all information about Chessman::Piece.
+        * @param Pointer to Chessman::Piece object.
+        */
+        void piece(Chessman::Piece*);
+
+        /**
+        * @brief Print all information about Chessboard::Board.
+        * @param Pointer to Chessboard::Board object.
+        */
+        void operator<<(Chessboard::Board*);
+        /**
+        * @brief Print all information about Chessman::Piece.
+        * @param Pointer to Chessman::Piece object.
+        */
+        void operator<<(Chessman::Piece*);
+    };
+}
+
+#endif // DUMP_HPP
