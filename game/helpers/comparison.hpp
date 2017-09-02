eac54f7f349411853cac03e6e083038718535966
edwinkepler <edwinkepler@protonmail.com>
2017-09-02 11:55:11 +0200

    Add comparison helpers functions

---
+++ game/helpers/comparison.hpp
@@ -0,0 +1,15 @@
+#ifndef COMPARISON_HPP
+#define COMPARISON_HPP
+
+#include <vector>
+#include <utility>
+#include <tuple>
+
+using namespace std;
+
+namespace Helper
+{
+    bool contains(const vector<tuple<int, int, int>>&, const pair<int, int>&);
+}
+
+#endif // COMPARISON_HPP
