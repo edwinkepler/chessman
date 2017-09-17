c4b7e0f9b3b203e7d662d466e5fe7bdca1e361df
edwinkepler <edwinkepler@protonmail.com>
2017-09-17 12:29:12 +0200

    Add AI::KIPP class

---
+++ game/ai/kipp.cpp
@@ -0,0 +1,1 @@
+#include "kipp.hpp"
---
+++ game/ai/kipp.hpp
@@ -0,0 +1,39 @@
+/**
+ * @copyright 2017 Edwin Kepler
+ * @license MIT
+ */
+
+#ifndef KIPP_HPP
+#define KIPP_HPP
+
+#include "debug/log.hpp"
+#include "pieces/piece.hpp"
+#include "board.hpp"
+
+using namespace std;
+
+/**
+ * @brief This namespace contains everything AI related.
+ */
+namespace AI
+{
+    /**
+     * @brief   Simple chess engine.
+     */
+    class KIPP {
+    public:
+        /**
+         * @brief Constructor
+         */
+        KIPP();
+        /**
+         * @brief Destructor.
+         */
+        ~KIPP();
+
+    private:
+
+    };
+}
+
+#endif // KIPP_HPP
